7c1ed614cd481fb68a2b92edc08034ac67ef74db
unknown <arshadaman202@gmail.com>
2021-08-06 17:26:17 +0530

    Initial commit

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 from flask import Flask, request
 from flask.templating import render_template
 from flask_sqlalchemy import SQLAlchemy
+import os
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/test.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
 db = SQLAlchemy(app)
 
 
